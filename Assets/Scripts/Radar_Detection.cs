90f34bc8e939b6e62db734cac25408ef726d670d
paul0989 <paul9740iii@gmail.com>
2019-07-24 18:45:54 +0800

    雷達.地圖改進
    雷達修正Enemy進入雷達範圍才顯示point

---
+++ Assets/Scripts/Radar_Detection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar_Detection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag== "EnemyCar_Point" || other.gameObject.tag == "EnemyTank_Point" )
+        {
+            other.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "EnemyCar_Point" || other.gameObject.tag == "EnemyTank_Point")
+        {
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+    }
+
+}
